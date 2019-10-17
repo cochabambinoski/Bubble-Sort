8f359670dc2388521c18de0f762286c73857b406
Feber <cochabambinoski@gmail.com>
2019-10-17 14:46:13 -0400

    Revert "fixing guard clause in rubocop second"
    This reverts commit 4f3373d6464a2f90c2d720707c780deaa658d260.

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
 def bubble_sort_by(arr)
-    unless block_given?
-      raise "no block"
+    if block_given?
+        swap = -1
+        while swap != 0
+            swap = 0
+            (0..(arr.length - 2)).each do |i|
+                if yield(arr[i], arr[i + 1]).positive?
+                    arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                    swap += 1
+                end
+            end
+        end
     else
-      swap = -1
-      while swap != 0
-          swap = 0
-          (0..(arr.length - 2)).each do |i|
-              if yield(arr[i], arr[i + 1]).positive?
-                  arr[i], arr[i + 1] = arr[i + 1], arr[i]
-                  swap += 1
-              end
-          end
-      end      
+        raise "no block"
     end
     arr
 end
