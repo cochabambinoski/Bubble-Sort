62bb4aaa33dcb2d780eee0c0c1c07d3fb6c2944c
Feber <cochabambinoski@gmail.com>
2019-10-17 15:07:20 -0400

    fixing guard clause in rubocop identation for guard

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -2,16 +2,16 @@
 
 def bubble_sort_by(arr)
     raise "no block" unless block_given?
-        swap = -1
-        while swap != 0
-            swap = 0
-            (0..(arr.length - 2)).each do |i|
-                if yield(arr[i], arr[i + 1]).positive?
-                    arr[i], arr[i + 1] = arr[i + 1], arr[i]
-                    swap += 1
-                end
+    swap = -1
+    while swap != 0
+        swap = 0
+        (0..(arr.length - 2)).each do |i|
+            if yield(arr[i], arr[i + 1]).positive?
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swap += 1
             end
         end
+    end
     
     arr
 end
